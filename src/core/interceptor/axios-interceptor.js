077901ccfd39decdacb90bfec79828a54b94de16
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-06-03 02:02:42 +0700

    feat(app): implement signature on frontend

--- src/core/interceptor/axios-interceptor.js
+++ src/core/interceptor/axios-interceptor.js
@@ -2,8 +2,47 @@ import axios from "axios";
 import { showErrorToast, showConfirmationDialog } from "@/helpers/alert";
 import { useAuthStore } from "@/state/pinia";
 import { getProgressInstance } from "@/helpers/progress"; // Pastikan helper ini ada
+import { JSEncrypt } from 'jsencrypt';
+import CryptoJS from 'crypto-js';
+// import { key } from '@/key';
 
-export function axiosInterceptors() {
+export function axiosInterceptors(api) {
+    // const privateKey = key.private;
+    // const publicKey = key.public;
+
+    // api.interceptors.request.use(config => {
+    //     if (['POST', 'PUT', 'DELETE'].includes(config.method.toUpperCase())) {
+    //         let dataString = JSON.stringify(config.data);
+    //         if (config.method.toUpperCase() === 'DELETE') {
+    //             const id = config.url.split('/').pop();
+    //             dataString = JSON.stringify({ id });
+    //         }
+
+    //         const signer = new JSEncrypt();
+    //         signer.setPrivateKey(privateKey);
+    //         const signature = signer.sign(dataString, CryptoJS.SHA256, 'sha256');
+    //         config.headers['signature'] = signature;
+    //     }
+    //     return config;
+    // });
+
+    // api.interceptors.response.use(response => {
+    //     if (response.headers['signature']) {
+    //         const verifier = new JSEncrypt();
+    //         verifier.setPublicKey(publicKey);
+    //         const valid = verifier.verify(
+    //             JSON.stringify(response.data.data),
+    //             response.headers['signature'],
+    //             CryptoJS.SHA256
+    //         );
+    //         if (!valid) {
+    //             alert('Signature not valid. Data may be tampered.');
+    //             throw new Error('Signature verification failed');
+    //         }
+    //     }
+    //     return response;
+    // });
+
     setTimeout(() => { // Pastikan store siap sebelum dipakai
         const auth = useAuthStore();
         const progress = getProgressInstance(); // Ambil instance progress
